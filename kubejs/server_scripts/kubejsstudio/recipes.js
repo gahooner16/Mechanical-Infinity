7e333507b88fa199f9c0603f2919d9385551e93f
SolarDaisy <137563041+SolarDaisy@users.noreply.github.com>
2026-09-08 15:32:40 +1200

    Update recipes.js
    changed my mind, it's going in a new file

--- kubejs/server_scripts/kubejsstudio/recipes.js
+++ kubejs/server_scripts/kubejsstudio/recipes.js
@@ -4,29 +4,3 @@ ServerEvents.generateData('after_mods', event => {
     event.json('sfm:recipe/cable', {"type":"minecraft:crafting_shaped","category":"misc","pattern":["#A#","BCB","#A#"],"key":{"#":{"item":"kubejs:ender_essence"},"A":{"item":"minecraft:light_weighted_pressure_plate"},"B":{"item":"minecraft:iron_bars"},"C":{"tag":"c:chests"}},"result":{"id":"sfm:cable","count":16}})
 })
 
-const matterTypes = global.MatterBlockConfig || [];
-
-ServerEvents.recipes(event => {
-    matterTypes.forEach(function (entry) {
-        var key = entry.key;
-        var maxCompression = entry.maxCompression || 5;
-
-        for (var tier = 0; tier < maxCompression; tier++) {
-            var currentId = tier === 0 ? key + '_essence_block' : key + '_essence_block_' + tier;
-            var nextId = key + '_essence_block_' + (tier + 1);
-            var compressId = 'kubejs:' + key + '_compress_' + tier + '_to_' + (tier + 1);
-            var decompressId = 'kubejs:' + key + '_decompress_' + (tier + 1) + '_to_' + tier;
-
-            event.shaped(Item.of(nextId, 1), [
-                '###',
-                '#A#',
-                '###'
-            ], {
-                '#': currentId,
-                A: currentId
-            }).id(compressId);
-
-            event.shapeless(Item.of(currentId, 9), [nextId]).id(decompressId);
-        }
-    });
-});
