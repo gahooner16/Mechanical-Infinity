0a64c78a74c62ec9da478bd7b1fdb67d8cb8d370
SolarDaisy <137563041+SolarDaisy@users.noreply.github.com>
2026-09-08 16:04:10 +1200

    Update InfinityCells.js

--- kubejs/startup_scripts/ae2/extendedae/InfinityCells.js
+++ kubejs/startup_scripts/ae2/extendedae/InfinityCells.js
@@ -1,18 +1,35 @@
 StartupEvents.registry('item', MR => {
 
-    MR.create('lava_cell', 'custom_infinity_cell')
-        .texture('kubejs:item/lava_cell')
-        .fluidType('minecraft:lava')
-        .cellModel('kubejs:block/drive/lava_cell');
+    [
+        { item: 'kubejs:earth_essence' },
+        { item: 'kubejs:metallic_essence' },
+        { item: 'kubejs:ender_essence' },
+        { item: 'kubejs:nether_essence' },
+        { item: 'kubejs:organic_essence' },
+        { item: 'kubejs:precious_essence' },
+        { item: 'kubejs:living_essence' },
+        { item: 'kubejs:nuclear_essence' },
+        { item: 'kubejs:plasma_essence' },
+        { item: 'kubejs:draconic_essence' },
+        { item: 'kubejs:chaotic_essence' },
+        { item: 'kubejs:mystic_essence' },
+        { item: 'kubejs:chrono_essence' },
+        { item: 'kubejs:quantum_essence' },
 
-    MR.create('water_cell', 'custom_infinity_cell')
-        .texture('kubejs:item/water_cell')
-        .fluidType('minecraft:water')
-        .cellModel('kubejs:block/drive/water_cell');
+        // Add entries using one of these properties:
+        // { item: 'minecraft:iron_ingot' },
+        // { block: 'minecraft:stone' },
+        // { fluid: 'minecraft:water' },
+    ].forEach(cell => {
+        const type = Object.keys(cell)[0];
+        const target = cell[type];
+        const [namespace, path] = target.split(':');
+        const textureType = type === 'block' ? 'block' : 'item';
 
-    MR.create('cobblestone_cell', 'custom_infinity_cell')
-        .texture('kubejs:item/cobblestone_cell')
-        .itemType('minecraft:cobblestone')
-        .cellModel('kubejs:block/drive/cobblestone_cell');
+        MR.create(`${path}_cell`, 'custom_infinity_cell')
+            .texture(`${namespace}:${textureType}/${path}`)
+            [`${type}Type`](target)
+            .cellModel(`kubejs:block/drive/${path}_cell`);
+    });
 
 })
